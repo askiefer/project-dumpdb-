f1afa9df9acc00ef904140bb4a72814eef18b68c
Anna Kiefer <annaskiefer@gmail.com>
2016-05-16 16:49:26 -0700

    Added sites by state and site details page

--- server.py
+++ server.py
@@ -19,19 +19,21 @@ def index():
 
     return render_template("base.html")
 
-@app.route('/search/<int:zipcode>')
-def site_search(zipcode):
-    """Retrieves site details given zipcode and waste type"""
+# FIXME create an algorithm that calculates nearest site given zipcode
+# @app.route('/search/<int:zipcode>')
+# def site_search(zipcode):
+#     """Retrieves site details given zipcode"""
 
-    zipcode = int(request.args.get("zipcode"))
-    user_input = request.args.get("waste")
-    # query the db for the site id requested
-    user_site = Site.query.filter(site_type=user_input, Site_zipcode=zipcode).first()
-    # if the site exists in the db
-    if user_site:
-        return jsonify(user_site)
-    else:
-        flash("The zipcode you entered is not in dump_db. Please enter another.")
+#     zipcode = int(request.args.get("zipcode"))
+
+#     # queries the db for the site object given the requested zipcode
+#     user_site = Site.query.filter(site_zipcode=zipcode).first()
+
+#     # if the site exists in the db
+#     if user_site:
+#         return jsonify(json_list=[i.serialize for i in user_site])
+#     else:
+#         flash("The zipcode you entered is not in dump_db. Please enter another.")
 
 @app.route('/sites')
 def create_map():
@@ -42,43 +44,28 @@ def create_map():
 @app.route('/sites_json')
 def json_sites():
 
-    # query the db for the site objects
+    # queries the db for the site objects
     sites = Site.query.all()
     return jsonify(json_list=[i.serialize for i in sites])
 
-    # # iterates through the list of objects and creates a dictionary for each
-    # for site in sites:
-    #     json_sites[site.site_id] = {
-    #                 "siteType": site.site_type,
-    #                 "siteName": site.site_name,
-    #                 "ownershipType": site.ownership_type,
-    #                 "yearOpened": site.year_opened,
-    #                 "closureYear": site.closure_year,
-    #                 "latitude": site.latitude,
-    #                 "longitude": site.longitude
-    #             }
+@app.route('/stateList')
+def by_state():
+    """A list of sites for a given state"""
 
-    # makes site_list a dictionary with key "sites" to jsonify
+    state = request.args.get("state")
 
+    # returns a list of tuples with name and owner for each site
+    state_sites = (db.session.query(Site.site_id, Site.site_name, Site.ownership_organization).filter(Site.site_state==state).order_by('site_name').all())
+    return render_template("state_list.html", sites=state_sites)
 
-# @app.route('/sites.json')
-# def site_info():
-#     """JSON information about sites."""
+@app.route('/sites/<int:site_id>')
+def site_details(site_id):
+    """Details of a single site"""
 
-#     sites = Site.query.limit(50)
-#     for site in sites:
-#         site = {
-#             site.site_id: {
-#                 "siteType": site.site_type,
-#                 "siteName": site.site_name,
-#                 "ownershipType": site.ownership_type,
-#                 "yearOpened": site.year_opened,
-#                 "closureYear": site.closure_year,
-#                 "latitude": site.latitude,
-#                 "longitude": site.longitude
-#                 }}
-#         return jsonify(sites)
-
+    # returns the site object
+    site = (db.session.query(Site).filter(Site.site_id==site_id).first())
+    print site
+    return render_template("site_details.html", site=site)
 
 #---------------------------------------------------------------------#
 
